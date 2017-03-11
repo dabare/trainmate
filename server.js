83059cc48ce6356cc59c2a93976d82dd2e6c0c27
Kasun <kmadushanka40@gmail.com>
2017-03-11 21:57:28 +0530

    first commit

--- server.js
+++ server.js
@@ -10,6 +10,47 @@ var portName = 'COM5'; //change this to your Arduino port
 var sendData = "";
 
 // handle contains locations to browse to (vote and poll); pathnames.
+function startServer(route,handle,debug)
+{
+	// on request event
+	function onRequest(request, response) {
+	  // parse the requested url into pathname. pathname will be compared
+	  // in route.js to handle (var content), if it matches the a page will
+	  // come up. Otherwise a 404 will be given.
+	  var pathname = url.parse(request.url).pathname;
+	  console.log("Request for " + pathname + " received");
+	  var content = route(handle,pathname,response,request,debug);
+	}
 
+	var httpServer = http.createServer(onRequest).listen(, function(){
+		console.log("Listening at: http://localhost:1337");
+		console.log("Server is up");
+	});
+	//serialListener(debug);
+	//initSocketIO(httpServer,debug);
+}
 
-//exports.start = startServer;
+function initSocketIO(httpServer,debug)
+{
+	socketServer = socketio.listen(httpServer);
+	if(debug == false){
+		socketServer.set('log level', 1); // socket IO debug off
+	}
+
+	socketServer.on('connection', function (socket) {
+	//console.log('connected');
+	socket.emit('onconnection', {pollOneValue:sendData});
+	socketServer.on('update', function(data) {
+	socket.emit('updateData',{pollOneValue:data});
+	});
+	socket.on('message', function(data) {
+
+		socketServer.emit('updates',data);
+
+	});
+
+    });
+}
+
+// Listen to serial port
+exports.start = startServer;
