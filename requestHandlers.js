90c83a763cadcb964b7672715fd48a7024211368
Kasun <kmadushanka40@gmail.com>
2017-03-11 21:45:37 +0530

    first commit

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,8 +2,8 @@
 // typeoff handle[pathname] === a function in requestHandlers.
 // the handle and function are discribed in index.js
 
-var fs = require('fs')
-//server = require('./server');
+var fs = require('fs'),
+server = require('./server');
 
 function sendInterface(response) {
   console.log("Request handler 'interface' was called.");
