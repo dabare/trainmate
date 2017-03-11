3f257890126179199df5d277321579d52ed16ae3
Kasun <kmadushanka40@gmail.com>
2017-03-11 18:13:05 +0530

    first commit

--- server.js
+++ server.js
@@ -1,91 +1,11 @@
-var fs = require('fs')
-,http = require('http'),
-socketio = require('socket.io'),
-url = require("url"),
-SerialPort = require("serialport")
+var server = require("./server1");
+var router = require("./route");
+var requestHandlers = require("./requestHandlers");
 
-var socketServer;
-var serialPort;
-var portName = 'COM5'; //change this to your Arduino port
-var sendData = "";
+var debug = false;
 
-http.createServer(function (req, res) {
+var handle = {}
+handle["/"] = requestHandlers.sendInterface;
+handle["/interface"] = requestHandlers.sendInterface;
 
-	res.writeHead(200, { 'Content-Type': 'text/html' });
-	res.end('Hello, world!');
-
-}).listen(process.env.PORT || 8080);
-// handle contains locations to browse to (vote and poll); pathnames.
-function startServer(route,handle,debug)
-{
-	// on request event
-	function onRequest(request, response) {
-	  // parse the requested url into pathname. pathname will be compared
-	  // in route.js to handle (var content), if it matches the a page will
-	  // come up. Otherwise a 404 will be given.
-	  var pathname = url.parse(request.url).pathname;
-	  console.log("Request for " + pathname + " received");
-	  var content = route(handle,pathname,response,request,debug);
-	}
-
-
-
-/*var httpServer = http.createServer(onRequest).listen(process.env.PORT || 8080, function(){
-		console.log("Listening at: http://localhost:1337");
-		console.log("Server is up");
-	});*/
-	//serialListener(debug);
-	initSocketIO(httpServer,debug);
-}
-
-function initSocketIO(httpServer,debug)
-{
-	socketServer = socketio.listen(httpServer);
-	if(debug == false){
-		socketServer.set('log level', 1); // socket IO debug off
-	}
-
-	socketServer.on('connection', function (socket) {
-	//console.log('connected');
-	socket.emit('onconnection', {pollOneValue:sendData});
-	socketServer.on('update', function(data) {
-	socket.emit('updateData',{pollOneValue:data});
-	});
-	socket.on('message', function(data) {
-
-		socketServer.emit('updates',data);
-
-	});
-
-    });
-}
-
-// Listen to serial port
-function serialListener(debug)
-{
-    var receivedData = "";
-    serialPort = new SerialPort(portName, {
-        baudrate: 9600,
-        // defaults for Arduino serial communication
-         dataBits: 8,
-         parity: 'none',
-         stopBits: 1,
-         flowControl: false
-    });
-
-    serialPort.on("open", function () {
-      console.log('open serial communication');
-            // Listens to incoming data
-        serialPort.on('data', function(data) {
-             receivedData += data.toString();
-          if (receivedData .indexOf('E') >= 0 && receivedData .indexOf('B') >= 0) {
-           sendData = receivedData .substring(receivedData .indexOf('B') + 1, receivedData .indexOf('E'));
-           receivedData = '';
-         }
-         // send the incoming data to browser with websockets.
-       socketServer.emit('update', sendData);
-      });
-    });
-}
-
-//exports.start = startServer;
+server.start(router.route,handle,debug);
