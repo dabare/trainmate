6d2f08b2570d5adee2bcd7056f82309975f93dd5
Kasun <kmadushanka40@gmail.com>
2017-03-11 21:15:34 +0530

    first commit

--- index.js
+++ index.js
@@ -9,3 +9,12 @@
 //handle["/interface"] = requestHandlers.sendInterface;
 
 //server.start(router.route,handle,debug);
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello, world!');
+
+}).listen(process.env.PORT || 8080);
