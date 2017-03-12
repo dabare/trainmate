3958a29bacc48deeace2f85478e9d7a1bb5c854b
Kasun <kmadushanka40@gmail.com>
2017-03-12 14:03:38 +0530

    first commit

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ function startServer(route,handle,debug)
 	}
 
 	var httpServer = http.createServer(onRequest).listen(process.env.PORT ||80, function(){
-		console.log("Listening at: http://localhost:1337");
+		console.log("Listening at: http://localhost:80");
 		console.log("Server is up");
 	});
 	//serialListener(debug);
